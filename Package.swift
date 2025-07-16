bd64a5a41b0085990b9c8f16165a0941aa98ef21
Roman-Fedotov-iOS <85881535+Roman-Fedotov-iOS@users.noreply.github.com>
2025-07-17 01:21:01 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
 //        )
     ],
     dependencies: [
-        .package(name: "DeltaCore", url: "https://github.com/rileytestut/DeltaCore.git", .branch("main")),
+        .package(name: "DeltaCore", url: "https://github.com/rileytestut/DeltaCore.git", .exect("0.1.5")),
     ],
     targets: [
         .target(
